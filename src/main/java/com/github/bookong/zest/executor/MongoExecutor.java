0b9f500ee25c193b61be296c206e92d77e21d00e
jiangxu <jiang.xu@bestv.com.cn>
2020-08-21 16:52:39 +0800

    0.2.26-SNAPSHOT

---
+++ src/main/java/com/github/bookong/zest/executor/MongoExecutor.java
@@ -0,0 +1,27 @@
+package com.github.bookong.zest.executor;
+
+import com.github.bookong.zest.runner.ZestWorker;
+import com.github.bookong.zest.testcase.AbstractTable;
+import com.github.bookong.zest.testcase.Source;
+import com.github.bookong.zest.testcase.ZestData;
+
+/**
+ * @author Jiang Xu
+ */
+public class MongoExecutor extends AbstractExecutor {
+
+    @Override
+    protected void init(ZestWorker worker, ZestData zestData, Source source, AbstractTable data) {
+
+    }
+
+    @Override
+    protected void verify(ZestWorker worker, ZestData zestData, Source source, AbstractTable data) {
+
+    }
+
+    @Override
+    public void clear(ZestWorker worker, ZestData zestData, Source source) {
+
+    }
+}
